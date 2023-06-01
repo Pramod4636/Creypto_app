866859904ccda2d42c4ee1c609ba80f673024766
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-01 19:18:25 +0530

    fetching data show loading

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,26 +1,45 @@
-import React , { useEffect } from 'react'
+import React , { useState,useEffect } from 'react'
 import axios from 'axios'
 import {server} from '../index'
+import { Container } from '@chakra-ui/react';
+import Loader from './Loader'
+import { HStack } from '@chakra-ui/react';
 
 const Exchanges = () => {
- 
+    
+    const [exchanges,setExchanges] = useState([]);
+    const [loading ,setLoading] = useState(true);
+
+    // data is fetched 
     useEffect( ()=>{
-       
-       
         const fetchExchange = async () => {
-            const {data} = await axios.get(`${server}/exchanges`); 
+            const {data} = await axios.get(`${server}/exchanges?per_page=250`); 
             console.log(data);
-        
+            setExchanges(data);
+            setLoading(false);
         };
         fetchExchange();
     } 
+   
+
+
     ,[]);
 
 
   return (
-    <div>
-      Exchanges
-    </div>
+    <Container maxW = {"container.xl"}>
+        {loading ? <Loader/> :  <>
+          
+          <Hstack>
+            {
+                exchanges.map( (i) => 
+                {
+
+                })
+            }
+          </Hstack>
+        </>}
+    </Container>
   );
 }
 
