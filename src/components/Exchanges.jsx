a1f588dc9c212a51b3c1f364641ffa6fc673b090
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-01 18:59:32 +0530

    data for exchange is fetched from api sucessfuly

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React , { useEffect } from 'react'
+import axios from 'axios'
+import {server} from '../index'
 
 const Exchanges = () => {
+ 
+    useEffect( ()=>{
+       
+       
+        const fetchExchange = async () => {
+            const {data} = await axios.get(`${server}/exchanges`); 
+            console.log(data);
+        
+        };
+        fetchExchange();
+    } 
+    ,[]);
+
+
   return (
     <div>
-      <h2>Exchanges</h2>
+      Exchanges
     </div>
-  )
+  );
 }
 
 export default Exchanges
--- src/index.js
+++ src/index.js
@@ -12,3 +12,5 @@ root.render(
     
   </React.StrictMode>
 );
+
+export const server = 'https://api.coingecko.com/api/v3/'
