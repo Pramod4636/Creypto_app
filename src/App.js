0a3cb8bcaa968882cedb397f247a8323c361e753
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-01 18:16:41 +0530

    header is aded all routes are set and imported all object to app .js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-
 import React from 'react'
-
+import Header from './components/Header'
+import Home from './components/Home'
+import Coins from './components/Coins'
+import CoinDetails from './components/CoinDetails'
+import Exchanges from './components/Exchanges'
 function App()
 {
     return ( 
       <Router>
+
+        <Header/>
         <Routes>
-            <Route/>
+            <Route path="/"  element={<Home />} />
+            <Route path="/coins"  element={<Coins />} />
+            <Route path="/exchnages"  element={<Exchanges />} />
+            <Route path="/coin/:id"  element={<CoinDetails />} />
+            
         </Routes>
       </Router>
     
